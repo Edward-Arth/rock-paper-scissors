5d4e86e35eba7472142cac09e914e0d17e1f5741
Edward-Arth <116842980+Edward-Arth@users.noreply.github.com>
2022-11-23 21:41:51 -0500

    Created game function

--- index.js
+++ index.js
@@ -7,14 +7,42 @@ function playRound(playerSelection, computerSelection){
     let player = prompt('rock, paper, scissors, shoot!');
     let computer = getComputerChoice();
     if (player===computer) {
-        alert('you tied')
+        return('tie');
     }
     else if (player === 'scissors' && computer === 'rock' || player === 'rock' && computer === 'paper' || player === 'paper' && computer === 'scissors') {
-        alert('you lose!')
+        return ('computerScore');
     }
     else if (player === 'rock' && computer === 'scissors' || player === 'paper' && computer === 'rock' || player === 'scissors' && computer === 'paper') {
-        alert('you win!')
+        return('playerScore');
     }
 }
 
-playRound();
+function game() {
+    let playerPoints = 0;
+    let computerPoints = 0;
+    for (let i = 0; i < 5; i++){
+        let point = playRound();
+        if (point === 'tie'){
+            'tie';
+        }
+        else if (point === 'computerScore'){
+            computerPoints = computerPoints++;
+            computerPoints++;
+        }
+        else if (point === 'playerScore'){
+            playerPoints = playerPoints++;
+            playerPoints++;
+        }
+    }
+    if (playerPoints > computerPoints) {
+        alert('YOU WIN!')
+    }
+    else if (computerPoints > playerPoints){
+        alert('you kinda suck')
+    }
+    else if (computerPoints === playerPoints){
+        alert('you tied')
+    }
+}
+
+game();
