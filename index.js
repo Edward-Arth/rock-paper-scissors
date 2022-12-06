060dedfae23e05d9de681e0ef1455deafe78ffd4
Edward-Arth <116842980+Edward-Arth@users.noreply.github.com>
2022-12-06 15:44:14 -0500

    Cleaned up game mechanics and styled webpage

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ function getComputerChoice(){
 
 let playerScore = 0;
 let computerScore = 0;
+let tieScore = 0;
 
 let scoreB1 = document.querySelector('#pScore');
 scoreB1.textContent = 'Your bananas: ' + playerScore;
 
 let scoreB2 = document.querySelector('#cScore');
-scoreB2.textContent = 'Computer bananas: ' + computerScore;
+scoreB2.textContent = 'Cyborg gorilla\'s bananas: ' + computerScore;
+
+let scoreB3 = document.querySelector('#tScore');
+scoreB3.textContent = 'Bananas too bruised to eat: ' + tieScore;
 
 function playRound(){
     let computer = getComputerChoice();
     if (player===computer) {
-        console.log('TIE');
+        tieScore += 1;
     }
     else if (player === 'scissors' && computer === 'rock' || player === 'rock' && computer === 'paper' || player === 'paper' && computer === 'scissors') {
         computerScore += 1;
@@ -24,24 +28,44 @@ function playRound(){
         playerScore += 1;
     }
     scoreB1.textContent = 'Your bananas: ' + playerScore;
-    scoreB2.textContent = 'Computer bananas: ' + computerScore;
+    scoreB2.textContent = 'Cyborg gorilla\'s bananas: ' + computerScore;
+    scoreB3.textContent = 'Bananas too bruised to eat: ' + tieScore;
+
+    if (scoreB1.textContent === 'Your bananas: 5'){
+        if (confirm('You won the clash...Fight again?')){
+            location.reload();
+        }
+        else {
+            scoreB1.textContent = 'You flee with your bananas...';
+            scoreB2.textContent = '';
+            scoreB3.textContent = '';
+            btn1.disabled = true, btn2.disabled = true, btn3.disabled = true;
+        }
+    }
+    else if (scoreB2.textContent === 'Cyborg gorilla\'s bananas: 5'){
+        if (confirm('You lost the clash...Fight again?')){
+            location.reload();
+        }
+        else {
+            scoreB1.textContent = 'The alpha gorilla wanders off with your bananas...';
+            scoreB2.textContent = '';
+            scoreB3.textContent = '';
+            btn1.disabled = true, btn2.disabled = true, btn3.disabled = true;
+        }
+    }
 };
 
-function game(){
-    let btn1 = document.querySelector('#rk');
-        btn1.addEventListener('click', () => {
-            playRound(player = 'rock');
+let btn1 = document.querySelector('#rk');
+    btn1.addEventListener('click', () => {
+        playRound(player = 'rock');
     });
 
-    let btn2 = document.querySelector('#pp');
-        btn2.addEventListener('click', () => {
-            playRound(player = 'paper');
+let btn2 = document.querySelector('#pp');
+    btn2.addEventListener('click', () => {
+        playRound(player = 'paper');
     });
 
-    let btn3 = document.querySelector('#sz');
-        btn3.addEventListener('click', () => {
-            playRound(player = 'scissors');
+let btn3 = document.querySelector('#sz');
+    btn3.addEventListener('click', () => {
+        playRound(player = 'scissors');
     });
-};
-
-game();
