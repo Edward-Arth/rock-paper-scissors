edbd999f46d469bbee4d19cf580022a03fda42a6
Edward-Arth <116842980+Edward-Arth@users.noreply.github.com>
2022-12-06 13:04:59 -0500

    Created RPS buttons and score display

--- index.js
+++ index.js
@@ -3,46 +3,45 @@ function getComputerChoice(){
     return random[Math.floor(Math.random() * 3)];
 };
 
-function playRound(playerSelection, computerSelection){
-    let player = prompt('rock, paper, scissors, shoot!');
+let playerScore = 0;
+let computerScore = 0;
+
+let scoreB1 = document.querySelector('#pScore');
+scoreB1.textContent = 'Your bananas: ' + playerScore;
+
+let scoreB2 = document.querySelector('#cScore');
+scoreB2.textContent = 'Computer bananas: ' + computerScore;
+
+function playRound(){
     let computer = getComputerChoice();
     if (player===computer) {
-        return('tie');
+        console.log('TIE');
     }
     else if (player === 'scissors' && computer === 'rock' || player === 'rock' && computer === 'paper' || player === 'paper' && computer === 'scissors') {
-        return ('computerScore');
+        computerScore += 1;
     }
     else if (player === 'rock' && computer === 'scissors' || player === 'paper' && computer === 'rock' || player === 'scissors' && computer === 'paper') {
-        return('playerScore');
+        playerScore += 1;
     }
-}
+    scoreB1.textContent = 'Your bananas: ' + playerScore;
+    scoreB2.textContent = 'Computer bananas: ' + computerScore;
+};
 
-function game() {
-    let playerPoints = 0;
-    let computerPoints = 0;
-    for (let i = 0; i < 5; i++){
-        let point = playRound();
-        if (point === 'tie'){
-            'tie';
-        }
-        else if (point === 'computerScore'){
-            computerPoints = computerPoints++;
-            computerPoints++;
-        }
-        else if (point === 'playerScore'){
-            playerPoints = playerPoints++;
-            playerPoints++;
-        }
-    }
-    if (playerPoints > computerPoints) {
-        alert('YOU WIN!')
-    }
-    else if (computerPoints > playerPoints){
-        alert('you kinda suck')
-    }
-    else if (computerPoints === playerPoints){
-        alert('you tied')
-    }
-}
+function game(){
+    let btn1 = document.querySelector('#rk');
+        btn1.addEventListener('click', () => {
+            playRound(player = 'rock');
+    });
+
+    let btn2 = document.querySelector('#pp');
+        btn2.addEventListener('click', () => {
+            playRound(player = 'paper');
+    });
+
+    let btn3 = document.querySelector('#sz');
+        btn3.addEventListener('click', () => {
+            playRound(player = 'scissors');
+    });
+};
 
 game();
